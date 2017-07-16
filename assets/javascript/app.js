33482885426a776cbf8e72c37e9ea3cc6fdb3ff9
David Jagodowski <davejag@comcast.net>
2017-07-15 23:47:46 -0400

    Major change in approach since previous version. Generating form in JS now then populating. Need to work on scoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
 
 // MAIN PROGRAM
   
-  var time = 10; // Sets length of gameplay. Initializes timer at 30 seconds.
+  var time = 20; // Sets length of gameplay. Initializes timer at 30 seconds.
   var intervalId;
   var timeUp = false;
+  var answered = false;
   var myChoice = "";
+  var numQuestions = 3; //default number of questions asked per game.
   var numCorrect = 0;
   var numIncorrect = 0;
 
@@ -61,6 +63,8 @@ $(document).ready(function() {
   };
 
   function loadStartScreen() { //Draws the START button and prompt screen.
+   // document.getElementById("contentDiv").addEventListener("load",)
+    $("#contentDiv").hide(); //Hides the content div on this screen
     $("#titleDiv").html('<p id="startTextId">' + 
     	'Click the START button to begin. </p>' +  
     	'<p style="text-align:center"> <img id="startButtonId" src="./assets/images/start_button.png"> </p>');
@@ -72,6 +76,7 @@ $(document).ready(function() {
     $("startButtonId").off("click");
     $("#startButtonId").click(function() { 
       console.log("loadGameScreen function called.");
+    $("#contentDiv").show(); //restore contentDiv  
 	  // $("#contentDiv").html(""); //clears the div content.
 	  $("#titleDiv").html("<h1>Totally Trivial Trivia!");
    	  //Display timer with 30 seconds on the clock
@@ -84,47 +89,86 @@ $(document).ready(function() {
 
   function showQuestion() {
     var space = "&nbsp; &nbsp; &nbsp; ";
-    var selection = "";
     console.log("function showQuestion called");
-    //write 1 question out to the contentDiv
-    $("#question").html("Question 1: " + questionArray[0].question);
-    
-    //write out possible answers to input/radio button element in the html
-    $("#answer1").html("A " + questionArray[0].answers.a);
-    $("#answer2").html("B " + questionArray[0].answers.b);
-    $("#answer3").html("C " + questionArray[0].answers.c);
-    $("#answer4").html("D " + questionArray[0].answers.d);
+    for (var i=0; i<numQuestions; i++) {
+  
 
+      //write question i out to the contentDiv
+      var divId = "#div" + i;
+      console.log("divId has value " + divId);
+      //$("#question").html('Question '+(i+1) + ": " + questionArray[i].question);
+      $(divId).prepend('<p>Question '+(i+1) + ": " + questionArray[i].question + '</p>'); 
+      // write out form element to div  
+      var formName = "q" + i; // gives each <form> element a unique name. 
+      var ans1 =  i + "answer1"; // 0answer1 -->gives each radio button a unique class. 
+      var ans2 =  i + "answer2"; // 0answer2
+      var ans3 =  i + "answer3"; // 0answer3 
+      var ans4 =  i + "answer4"; // 0answer4
+      console.log("ans1 = " + ans1);
+      console.log("ans2 = " + ans2);
+      console.log("ans3 = " + ans3);
+      console.log("ans4 = " + ans4);
+ 
+//Assign variables to each answer and read them into the form upon creation.
+      var choiceA = "A) " + questionArray[i].answers.a;
+      var choiceB = "B) " + questionArray[i].answers.b;
+      var choiceC = "C) " + questionArray[i].answers.c;
+      var choiceD = "D) " + questionArray[i].answers.d; 
 
-    $("#a1").click(function() {
-      console.log("button a is checked");
-      myChoice = "a";
-      checkAnswer();
-      return myChoice});
-    $("#a2").click(function() { 
-      console.log("button b is checked");
-      myChoice = "b";
-      checkAnswer();
-      return myChoice}); 
-    $("#a3").click(function() {
-      console.log("button c is checked");
-      myChoice = "c";
-      checkAnswer();
-      return myChoice});
-    $("#a4").click(function() {
-      console.log("button d is checked");
-      myChoice = "d";
-      checkAnswer();
-      return myChoice}); 
-    
-     
- /*   //when a choice is made, check the answer.
-    if ($("#a1").checked === true ) {console.log("button a is checked")};
-    if ($("#a2").checked === true ) {console.log("button b is checked")};
-    if ($("#a3").checked === true ) {console.log("button c is checked")};
-    if ($("#a4").checked === true ) {console.log("button d is checked")};
- */
-  }
+      $(divId).append('<form>' +
+      '<input type="radio" name=formName id="a1" value="a">' + choiceA + '<input type="radio" name=formName id="a2" value="b">' + choiceB +   
+      '<input type="radio" name=formName id="a3" value="c">' + choiceC + '<input type="radio" name=formName id="a4" value="d">' + choiceD + 
+      '</form>');
+        
+
+      //write out possible answers to input/radio button element in the html
+/*
+      var answerId1 = "."+ i + "answer1"; 
+      var answerId2 = "."+ i + "answer2"; 
+      var answerId3 = "."+ i + "answer3";
+      var answerId4 = "."+ i + "answer4";
+
+       console.log("answerId1 has value " + answerId1);
+       console.log("answerId2 has value " + answerId2);
+       console.log("answerId3 has value " + answerId3);
+       console.log("answerId4 has value " + answerId4);
+
+      $(answerId1).html("A) " + questionArray[i].answers.a);
+      $(answerId2).html("B) " + questionArray[i].answers.b);
+      $(answerId3).html("C) " + questionArray[i].answers.c);
+      $(answerId4).html("D) " + questionArray[i].answers.d);
+*/     
+   } //end for loop 
+
+      $("#a1").click(function() {
+        console.log("button a is checked");
+        myChoice = "a";
+        checkAnswer();
+        return myChoice}); 
+        
+ 
+      $("#a2").click(function() { 
+        console.log("button b is checked");
+        myChoice = "b";
+        checkAnswer();
+        return myChoice}); 
+
+      $("#a3").click(function() {
+        console.log("button c is checked");
+        myChoice = "c";
+        checkAnswer();
+        return myChoice}); 
+
+      $("#a4").click(function() {
+        console.log("button d is checked");
+        myChoice = "d";
+        checkAnswer();
+        return myChoice}); 
+  
+          
+    //  } // end while
+  } // end function showQuestion()
+
 
   function checkAnswer() {
   	console.log("function checkAnswer called.");
